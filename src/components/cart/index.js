3b2bb727edf2ad57c8327597446a7a2fb0643943
AndresDiPietro <belmonteandres12@gmail.com>
2021-04-10 18:58:30 -0300

    Cart cant y precio total

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -4,15 +4,16 @@ import {CartContext} from '../../context/CartContext'
 import CartItem from "../cartItem";
 
 const Cart = () => {
-    const {cart, removeItem, clear} = useContext(CartContext)
-
+    const {cart, removeItem, clear, calcularPrecio} = useContext(CartContext)
+    let precioTotal = calcularPrecio()
     return(
         <section>
             {cart.map(e=> <CartItem item={e.item} cantidad={e.quantity} key={e.item.id} remove={removeItem}/>)}
             
-            {cart.length!==0? <button onClick={clear}>Vaciar carrito </button>
+            {cart.length!==0? 
+            <div>TOTAL: {precioTotal}<button onClick={clear}>Vaciar carrito </button></div>
                 :
-            <Link to='/'><button>Ir a productos</button></Link>}
+            <div>Tu carrito está vacío<Link to='/'><button>Ir por productos</button></Link></div>}
         </section>
     )
 
