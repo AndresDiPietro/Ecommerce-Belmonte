a65fcd3e9300f03f0a8ab117cf56b26dff4ee063
AndresDiPietro <belmonteandres12@gmail.com>
2021-04-10 18:59:06 -0300

    Cart precio total

--- src/components/cartWidget/index.js
+++ src/components/cartWidget/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Link} from 'react-router-dom';
+import {CartContext} from '../../context/CartContext'
 import "./cartWidget.css";
 
 const CartWidget = (props) => {
+    const {calcularTotal} = useContext(CartContext)
+    let cantItems = calcularTotal()
     return(
-        <span className="car-widget"><Link to='/cart' className="car-widget__link"><i className="fab fa-opencart navbar__icon icon-cart"></i></Link></span>
+        <span className="car-widget"><Link to='/cart' className="car-widget__link">{cantItems}<i className="fab fa-opencart navbar__icon icon-cart"></i></Link></span>
     )
 }
 
--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -5,30 +5,58 @@ export const CartContext = React.createContext({})
 export const CartProvider = ({children}) => {
     
     const [cart, setCart] = useState([])
+    // const [totalItemsCart, setTotalItemsCart] = useState(0)
 
-    const addItem = (item, quantity) => {
-        let newItem = {item:item, quantity:quantity}
-
-        if(cart.some(e => e.item.id === newItem.item.id))console.log('el producto está siendo duplicado')
-        else cart.push(newItem)
+    const addItem = (newItem, newQuantity) => {
+        let {quantity = 0} = cart.find(e=> e.item.id === newItem.id) || {}
         
-        setCart(cart)
+        let newCart = cart.filter(e => e.item.id !== newItem.id)
+
+        setCart([...newCart, { item: newItem , quantity: quantity + newQuantity }])
+        
+        // calcularTotal(cart)//Cantidad de items en el cart
     } // agregar cierta cantidad de un ítem al carrito
 
     const removeItem = (itemId) => {
         let newCart = cart.filter(e=> e.item.id !== itemId)
-        setCart(newCart)  
+        setCart(newCart)
+        // calcularTotal(newCart) 
+
+        // totalItems(cart)//Cantidad de items en el cart
     } // Remover un item del cart usando su id
 
     const clear = () => {
+
         setCart([])
-    } // Remover todos los items
         
+        // setTotalItemsCart(0)//Cantidad de items en el cart
+    } // Remover todos los items
+
+    // const totalItems = (carrito) => {
+    //     let casiTotal = []
+    //     let total
+    //     carrito.map((e=> casiTotal.push(e.quantity)))
+    //     carrito>0 ? total = casiTotal.reduce((a,b)=>a+b) : total= casiTotal
+    //     // console.log(`Total de elem en el cart ${total}`)
+    //     setTotalItemsCart(total)
+    // }//Cantidad de items en el carr
+        
+    const calcularTotal = () => {
+        return cart.reduce((count, cantidad)=>{
+            return count + cantidad.quantity
+        },0)
+    }
+    const calcularPrecio = () => {
+        return cart.reduce((count, item)=>{
+            return count + item.quantity * item.item.price
+        },0)
+    }
+
         // const isInCart = (id) => {
             
         //     }
     return(
-        <CartContext.Provider value={{addItem, cart, removeItem, clear}}>
+        <CartContext.Provider value={{addItem, cart, removeItem, clear, calcularTotal, calcularPrecio}}>
             {children}
         </CartContext.Provider>
     )
