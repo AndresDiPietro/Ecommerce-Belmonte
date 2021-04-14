26b184ae00709d35f565db7a50396316df7b0729
AndresDiPietro <belmonteandres12@gmail.com>
2021-04-14 19:31:29 -0300

    Desafío Cart View

--- src/components/itemDetailContainer/index.js
+++ src/components/itemDetailContainer/index.js
@@ -2,24 +2,26 @@ import React, {useState, useEffect} from "react"
 import {useParams} from "react-router-dom"
 import Products from "../products.json"
 import ItemDetail from "../itemDetail"
+import Loading from '../loading'
 
 const ItemDetailContainer = () => {
 
     const [item, setItem] = useState(null)
     const {itemId} = useParams()
+    const [loader, setLoader] = useState(false)
    
     useEffect(()=>{
+        setLoader(true)
         const promise = new Promise((resolve) => {
             setTimeout(() => {
                 resolve(Products.filter(product=> product.id === parseInt(itemId) ))
+                setLoader(false)
             },2000)
         })
         promise.then((res)=>setItem(res[0]))
         return
     },[itemId])
-
-    return(
-        <ItemDetail item={item}/>
-    )
+    if(loader) return <Loading/>
+    else return <ItemDetail item={item}/>
 }
-export default ItemDetailContainer;
+export default ItemDetailContainer
--- src/components/itemListContainer/index.js
+++ src/components/itemListContainer/index.js
@@ -1,42 +1,49 @@
-import React, {useState, useEffect} from "react";
-import ItemList from "../itemList";
-import {useParams} from "react-router-dom";
-import Products from "../products.json";
-import "./itemListContainer.css";
+import React, {useState, useEffect} from "react"
+import ItemList from "../itemList"
+import {useParams} from "react-router-dom"
+import Products from "../products.json"
+import "./itemListContainer.css"
+import Loading from '../loading'
 
 const ItemListContainer = () => {
-    const [items, setItems] = useState([]);
-    const {categoryId} = useParams();
-
+    const [items, setItems] = useState([])
+    const {categoryId} = useParams()
+    const [loader, setLoader] = useState(false)
+    
     const callProducts = () => {
+        setLoader(true)
         const promise = new Promise((resolve) => {
             setTimeout(() => {
                 if(categoryId === undefined) resolve(Products)
                 else resolve(Products.filter(product=> product.category === categoryId))
+                setLoader(false)
             },2000)
         })
         promise.then((res)=>{
             setItems(res)
         })
     }
-
-    let title; 
+    
+    let title
     items.map(item=> item.category === categoryId? title = categoryId : title = "Aprovechá el Hot Sale!")
     
-    let classItemList;
+    let classItemList
     if(categoryId === undefined)classItemList = "item-list"
     else classItemList = "categoryItemsList"
     
     useEffect(()=>{
         callProducts()
-    },[categoryId]);
+    },[categoryId])
     
-    
-    return(
+    return(    
         <section className="itemListContainer">
             <h2 className="itemListContainer__title">{title}</h2>
+            {
+            loader? <Loading/>
+                :   
             <ItemList classItemList={classItemList} items={items}/>
-        </section>
+            }
+        </section>     
     )
 }
-export default ItemListContainer;
+export default ItemListContainer
