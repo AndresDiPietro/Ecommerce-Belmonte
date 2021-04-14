54690e93a8be016d86b3908287047e4027a64108
AndresDiPietro <belmonteandres12@gmail.com>
2021-04-14 08:07:43 -0300

    Desafío Cart view

--- src/components/cartWidget/index.js
+++ src/components/cartWidget/index.js
@@ -5,14 +5,17 @@ import "./cartWidget.css"
 
 const CartWidget = () => {
     const {calculateQuantity} = useContext(CartContext)
-
-    return(
-        <span className="car-widget">
-            <Link to='/cart' className="car-widget__link">
-                <span className="car-widget__count">{calculateQuantity()}</span>
-                <i className="fab fa-opencart navbar__icon icon-cart"></i>
-            </Link>
-        </span>
+    if(calculateQuantity()!=0){
+        return(
+            <span className="car-widget">
+                <Link to='/cart' className="car-widget__link">
+                    <span className="car-widget__count">{calculateQuantity()}</span>
+                    <i className="fab fa-opencart navbar__icon icon-cart"></i>
+                </Link>
+            </span>
+        )
+    }else return(
+        null
     )
 }
 export default CartWidget
